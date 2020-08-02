6f33fa5c30f02fce1860a1831a67a8b121dfb8c3
BhavanandhanRadhakrishnan <michlebhavan@gmail.com>
2020-08-02 17:46:50 +0530

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+bhavan="navs"
+if bhavan=="navs":
+   
+    print("yes they are best friends")
+   
+else :
+    print("they arenyt best friends")
+
+eg=5
+if eg>3 and eg<10:
+	print("AND block is true")
+
+if eg<3 and eg<10:
+	print("OR block is  true")
