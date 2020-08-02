36a464cc42a22de8bc2d25c69b4401a9935cea67
BhavanandhanRadhakrishnan <michlebhavan@gmail.com>
2020-08-02 17:40:18 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,8 @@
+eg=[0,1,2,3,4,5,6,7,8,9]
+print(str(eg[2:5]))
+print(str(eg[:]))
+print(str(eg[5:]))                       
+print(str(eg[:4]))
+print(str(eg[::3]))
+print(str(eg[::4]))
+print(str(eg[9:0:-5]))
